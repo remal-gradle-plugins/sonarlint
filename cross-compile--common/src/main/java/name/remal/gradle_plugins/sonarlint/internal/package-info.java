cd90f42938bdd9b91f727e9a80844c20b8580a61
Semyon Levin <levin.semen@gmail.com>
2024-01-08 15:59:04 -0800

    Fix build

--- cross-compile--common/src/main/java/name/remal/gradle_plugins/sonarlint/internal/package-info.java
+++ cross-compile--common/src/main/java/name/remal/gradle_plugins/sonarlint/internal/package-info.java
@@ -1,6 +1,8 @@
 @NonNullApi
+@Internal
 @ToolkitValueStyle
 package name.remal.gradle_plugins.sonarlint.internal;
 
 import name.remal.gradle_plugins.toolkit.ToolkitValueStyle;
 import org.gradle.api.NonNullApi;
+import org.jetbrains.annotations.ApiStatus.Internal;
