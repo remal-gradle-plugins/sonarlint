e783470c6a8c8efc29e5710a9f5bb0e5d0b0c5f8
Semyon Levin <levin.semen@gmail.com>
2024-12-12 17:08:22 -0800

    [release-later] Additional tests

--- src/test/java/name/remal/gradle_plugins/sonarlint/SonarLintPluginTest.java
+++ src/test/java/name/remal/gradle_plugins/sonarlint/SonarLintPluginTest.java
@@ -1,8 +1,12 @@
 package name.remal.gradle_plugins.sonarlint;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.packageNameOf;
+import static name.remal.gradle_plugins.toolkit.reflection.ReflectionUtils.unwrapGeneratedSubclass;
+import static name.remal.gradle_plugins.toolkit.testkit.ProjectValidations.executeAfterEvaluateActions;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+import name.remal.gradle_plugins.toolkit.testkit.TaskValidations;
 import org.gradle.api.Project;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,8 +22,18 @@ class SonarLintPluginTest {
     }
 
     @Test
-    void test() {
-        assertTrue(project.getPlugins().hasPlugin(SonarLintPlugin.class));
+    void pluginTasksDoNotHavePropertyProblems() {
+        project.getPluginManager().apply("java");
+
+        executeAfterEvaluateActions(project);
+
+        val taskClassNamePrefix = packageNameOf(SonarLintPlugin.class) + '.';
+        project.getTasks().stream()
+            .filter(task -> {
+                val taskClass = unwrapGeneratedSubclass(task.getClass());
+                return taskClass.getName().startsWith(taskClassNamePrefix);
+            })
+            .forEach(TaskValidations::assertNoTaskPropertiesProblems);
     }
 
 }
