f8aaa45f8dd04688acd0b88481e5189f944187c7
Semyon Levin <levin.semen@gmail.com>
2025-05-05 00:23:23 +0400

    Improve tests

--- src/componentTest/java/name/remal/gradle_plugins/sonarlint/internal/impl/AbstractSonarLintServiceComponentTest.java
+++ src/componentTest/java/name/remal/gradle_plugins/sonarlint/internal/impl/AbstractSonarLintServiceComponentTest.java
@@ -1,6 +1,7 @@
 package name.remal.gradle_plugins.sonarlint.internal.impl;
 
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static name.remal.gradle_plugins.sonarlint.ComponentTestConstants.CURRENT_MINOR_GRADLE_VERSION;
 import static name.remal.gradle_plugins.toolkit.testkit.TestClasspath.getTestClasspathFirstLevelLibraryNotations;
 import static name.remal.gradle_plugins.toolkit.testkit.TestClasspath.getTestClasspathLibraryFilePaths;
 
@@ -11,9 +12,11 @@ import java.util.List;
 import java.util.Set;
 import name.remal.gradle_plugins.sonarlint.SonarLintLanguage;
 import name.remal.gradle_plugins.sonarlint.internal.impl.AbstractSonarLintServiceParams.AbstractSonarLintServiceParamsBuilder;
+import name.remal.gradle_plugins.toolkit.testkit.MinTestableGradleVersion;
 import name.remal.gradle_plugins.toolkit.testkit.MinTestableJavaVersion;
 
 @MinTestableJavaVersion(17)
+@MinTestableGradleVersion(CURRENT_MINOR_GRADLE_VERSION)
 abstract class AbstractSonarLintServiceComponentTest {
 
     protected static <T extends AbstractSonarLintServiceParamsBuilder<?, ?>> T configureParamsBuilderBase(T builder) {
