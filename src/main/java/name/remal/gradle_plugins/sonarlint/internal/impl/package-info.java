37240061d7e3d57030a52c78bd6c12e49a051723
Semyon Levin <levin.semen@gmail.com>
2025-08-08 16:51:38 +0400

    fix build

--- src/main/java/name/remal/gradle_plugins/sonarlint/internal/impl/package-info.java
+++ src/main/java/name/remal/gradle_plugins/sonarlint/internal/impl/package-info.java
@@ -2,5 +2,5 @@
 @Internal
 package name.remal.gradle_plugins.sonarlint.internal.impl;
 
-import org.jspecify.annotations.NullMarked;
 import org.jetbrains.annotations.ApiStatus.Internal;
+import org.jspecify.annotations.NullMarked;
--- src/main/java/name/remal/gradle_plugins/sonarlint/internal/package-info.java
+++ src/main/java/name/remal/gradle_plugins/sonarlint/internal/package-info.java
@@ -4,5 +4,5 @@
 package name.remal.gradle_plugins.sonarlint.internal;
 
 import name.remal.gradle_plugins.toolkit.ToolkitValueStyle;
-import org.jspecify.annotations.NullMarked;
 import org.jetbrains.annotations.ApiStatus.Internal;
+import org.jspecify.annotations.NullMarked;
