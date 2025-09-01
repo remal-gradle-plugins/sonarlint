058eaa43213d507091408890f9b1058054cb864b
Semyon Levin <levin.semen@gmail.com>
2025-09-01 23:56:05 +0400

    client socket timeout

--- src/main/java/name/remal/gradle_plugins/sonarlint/internal/utils/RmiSocketFactory.java
+++ src/main/java/name/remal/gradle_plugins/sonarlint/internal/utils/RmiSocketFactory.java
@@ -1,14 +1,17 @@
 package name.remal.gradle_plugins.sonarlint.internal.utils;
 
+import static java.lang.Math.toIntExact;
 import static lombok.AccessLevel.PRIVATE;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.server.RMIClientSocketFactory;
 import java.rmi.server.RMIServerSocketFactory;
+import java.time.Duration;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +22,9 @@ import org.jspecify.annotations.Nullable;
 @NoArgsConstructor(access = PRIVATE, force = true)
 class RmiSocketFactory implements RMIClientSocketFactory, RMIServerSocketFactory, Serializable {
 
+    private static final Duration CONNECTION_TIMEOUT = Duration.ofSeconds(1);
+    private static final Duration READ_TIMEOUT = Duration.ofMinutes(30);
+
     private final InetAddress bindAddr;
 
     @Nullable
@@ -26,7 +32,9 @@ class RmiSocketFactory implements RMIClientSocketFactory, RMIServerSocketFactory
 
     @Override
     public Socket createSocket(String host, int port) throws IOException {
-        var clientSocket = new Socket(getBindAddr(), port);
+        var clientSocket = new Socket();
+        clientSocket.connect(new InetSocketAddress(getBindAddr(), port), toIntExact(CONNECTION_TIMEOUT.toMillis()));
+        clientSocket.setSoTimeout(toIntExact(READ_TIMEOUT.toMillis()));
         lastUsedPort = clientSocket.getPort();
         return clientSocket;
     }
